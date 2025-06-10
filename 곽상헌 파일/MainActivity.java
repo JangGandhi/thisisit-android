3827f646016af2d3cd4614de5a220924e6d3b4cb
ssangman1234 <heon7988@naver.com>
2025-06-10 17:21:17 +0900

    곽상헌 디스이스잇 파일

---
+++ 곽상헌 파일/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.thisisit;
+
+import android.os.Bundle;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+import androidx.viewpager2.widget.ViewPager2;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ViewPager2 viewPager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        viewPager = findViewById(R.id.viewPager);
+        viewPager.setAdapter(new FragmentStateAdapter(this) {
+            @NonNull
+            @Override
+            public Fragment createFragment(int position) {
+                if (position == 0) return new LibraryFragment();
+                else return new ProfileFragment();
+            }
+
+            @Override
+            public int getItemCount() {
+                return 2;
+            }
+        });
+    }
+}
